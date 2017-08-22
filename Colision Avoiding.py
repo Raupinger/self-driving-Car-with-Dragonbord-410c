36a32796f217ab04f59a5c09345e8c2f3c2b87d5
Raupinger <fgrauper@gmail.com>
2017-08-22 17:22:27 +0200

    Delete Colision Avoiding.py

--- Colision Avoiding.py
+++
@@ -1,7 +0,0 @@
-if CarControls.front_distance < CarControls.min_distance :
-	if CarControls.left_distance < (CarControls.min_distance + CarControls.car_width) :
-		CarControls.avoide(direction= "left") 
-	elseif CarControls.right_distance < (CarControls.min_distance + CarControls.car_width) :
-		CarControls.avoide(direction= "right") 
-	else 
-		CarControls.brake(priority= "height")
